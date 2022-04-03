be70ebd4e2024881c7a23a27b5647cfe3212431f
velco22 <velco22@gmail.com>
2022-04-03 12:40:07 +0200

    vytvorenie aktivity

---
+++ app/src/main/java/com/example/mtaa/ManageUsersActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mtaa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ManageUsersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_users)
+    }
+}
---
+++ app/src/main/java/com/example/mtaa/ScheduleMeetActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mtaa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScheduleMeetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule_meet)
+    }
+}
