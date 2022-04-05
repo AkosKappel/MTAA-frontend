e5b2efe0488d631d6234e55bea06956b7e679c35
velco22 <velco22@gmail.com>
2022-04-06 00:34:11 +0200

    action bar

--- app/src/main/java/com/example/mtaa/ScheduleMeetActivity.kt
+++ app/src/main/java/com/example/mtaa/ScheduleMeetActivity.kt
@@ -4,23 +4,43 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class ScheduleMeetActivity : AppCompatActivity() {
 
     private lateinit var btnManageUsers: Button
+    private lateinit var btnHome: TextView
+    private lateinit var btnBack: ImageView
+    private lateinit var btnProfile: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule_meet)
 
         btnManageUsers = findViewById(R.id.btnManageUsers)
+        btnHome = findViewById(R.id.btnHome)
+        btnProfile = findViewById(R.id.btnProfile)
+        btnBack = findViewById(R.id.btnBack)
 
         btnManageUsers.setOnClickListener {
             val intent = Intent(this, ManageUsersActivity::class.java)
             startActivity(intent)
         }
 
+        btnHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
+        btnBack.setOnClickListener {
+            finish()
+        }
+
+        btnProfile.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
